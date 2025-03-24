d86492aed5e980fabba2fe4ad96313d1c362b584
siddheshm303 <siddheshm303@gmail.com>
2025-03-24 20:50:57 +0530

    Added MONGO_URI debug log

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 export const connectDB = async () => {
   try {
+    console.log('Mongo URI:', process.env.MONGO_URI); // Debugging step
+
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI is undefined!');
+    }
+
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log('Error connecting to mongoDb: ', error.message);
-    process.exit(1); //Status code 1 is failure, 0 is success
+    console.log('Error connecting to MongoDB:', error.message);
+    process.exit(1);
   }
 };
