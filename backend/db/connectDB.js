c0bbc3cb927e29d05ddfb1f86569c97400dbda07
siddheshm303 <siddheshm303@gmail.com>
2025-03-24 20:59:20 +0530

    Fixed mongoose import issue

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,3 +1,8 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const connectDB = async () => {
   try {
     console.log('Mongo URI:', process.env.MONGO_URI); // Debugging step
